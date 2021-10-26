3f6a8e8451f1623cd2def731001ce927c4faad33
Virginia Lucia <vlucia@gmail.com>
2021-10-26 00:40:40 -0300

    Inicio do Projeto MATD02

---
+++ main.py
@@ -0,0 +1,47 @@
+import machine
+import hcsr04
+import utime as time
+
+sensor = hcsr04.HCSR04(trigger_pin=13,echo_pin=12,echo_timeout_us=1000000)  
+ledgreen = machine.Pin(15, machine.Pin.OUT)
+ledred = machine.Pin(4, machine.Pin.OUT)
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if topic == b'notification' and msg == b'received':
+      print('ESP received a notification message')
+
+def connect_and_subscribe():
+  global client_id, mqtt_server, topic_sub
+  client = MQTTClient(client_id, mqtt_server)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(topic_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+try:
+    client = connect_and_subscribe()
+except OSError as e:
+      restart_and_reconnect()   
+
+while True:
+    try:
+        client.check_msg()
+        distance = (sensor.distance_cm())
+        print (distance)
+        if distance <=6:
+            ledgreen.off()
+            ledred.on()
+        else:
+            ledgreen.on()
+            ledred.off()
+            time.sleep_ms(3000)  
+    except OSError as e:
+        restart_and_reconnect() 
+            
