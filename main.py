096de2c4c6a24b9712455a11fd49413c624f4a48
Virginia Lucia <vlucia@gmail.com>
2021-10-31 12:26:01 -0300

    atualizando main - funcionando OK com broker mqtt

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import machine
 import hcsr04
 import utime as time
 
-def main():
-sensor = hcsr04.HCSR04(trigger_pin=13,echo_pin=12,echo_timeout_us=1000000)  
-ledgreen = machine.Pin(15, machine.Pin.OUT)
-ledred = machine.Pin(4, machine.Pin.OUT)
-
 def sub_cb(topic, msg):
-    print((topic, msg))
-    if topic == b'notification' and msg == b'received':
-      print('ESP received a notification message')
+  print((topic, msg))
+  if topic == b'notification' and msg == b'received':
+    print('ESP received a notification message')
 
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub
@@ -27,22 +22,33 @@ def restart_and_reconnect():
   machine.reset()
 
 try:
-    client = connect_and_subscribe()
+  client = connect_and_subscribe()
 except OSError as e:
-      restart_and_reconnect()   
+  restart_and_reconnect()
 
+sensor = hcsr04.HCSR04(trigger_pin=13,echo_pin=12,echo_timeout_us=1000000)
+ledgreen = machine.Pin(15, machine.Pin.OUT)
+ledred = machine.Pin(4, machine.Pin.OUT)
+    
+    
 while True:
-    try:
-        client.check_msg()
-        distance = (sensor.distance_cm())
-        print (distance)
-        if distance <=6:
-            ledgreen.off()
-            ledred.on()
-        else:
-            ledgreen.on()
-            ledred.off()
-            time.sleep_ms(3000)  
-    except OSError as e:
-        restart_and_reconnect() 
-            
+  try:
+    client.check_msg()
+    if (time.time() - last_message) > message_interval:
+      sensor.distance_cm()       
+      msg = str(sensor.distance_cm())      
+      client.publish(topic_pub, msg)
+      last_message = time.time()
+      counter += 1
+         
+    distance = (sensor.distance_cm())
+    print (distance)
+    if distance <=6:
+        ledgreen.off()
+        ledred.on()
+    else:
+        ledgreen.on()
+        ledred.off()
+        time.sleep_ms(1000)     
+  except OSError as e:
+    restart_and_reconnect()
